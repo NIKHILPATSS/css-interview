3f939a983168a957ac6cbfc68f1b97afbde04c3a
Nikhil Patange <patangenikhil08@gmail.com>
2023-04-24 00:33:03 +0530

    css interview question

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Nav'>
+        <div className='round'></div>
+        <div className='menu'>
+          <div className='menu-item'></div>
+          <div className='menu-item'></div>
+          <div className='menu-item'></div>
+        </div>
+        <div className='round'></div>
+      </div>
+      <div className='Body'>
+        <div className='body-item'></div>
+        <div className='body-item'></div>
+        <div className='body-item'></div>
+        <div className='body-item'></div>
+      </div>
+      
     </div>
   );
 }
